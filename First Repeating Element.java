94a0e7eb19c401612561331951c6a1cc3bc7c487
Annie Valentina A <Annievalentina1307@gmail.com>
2026-01-24 19:21:18 +0530

    Create First Repeating Element.java

---
+++ First Repeating Element.java
@@ -0,0 +1,33 @@
+/*
+Given an array arr[], find the first repeating element. The element should occur more than once and the index of its first occurrence should be the smallest.
+
+Note:- The position you return should be according to 1-based indexing. 
+
+Examples:
+
+Input: arr[] = [1, 5, 3, 4, 3, 5, 6]
+Output: 2
+Explanation: 5 appears twice and its first appearance is at index 2 which is less than 3 whose first the occurring index is 3.
+Input: arr[] = [1, 2, 3, 4]
+Output: -1
+Explanation: All elements appear only once so answer is -1.
+Constraints:
+1 ≤ arr.size() ≤ 106
+0 ≤ arr[i] ≤ 106
+
+*/
+
+class Solution {
+    public static int firstRepeated(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    return i + 1; 
+                }
+            }
+        }
+        return -1;
+    }
+}
